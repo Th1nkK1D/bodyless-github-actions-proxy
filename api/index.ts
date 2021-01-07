6a49dcdb5475cd8f8aaab83f1de04ae2d8e065ca
Th1nkK1D <witheep@gmail.com>
2021-01-07 15:47:17 +0700

    Add proxy logic in index endpoint

--- api/index.ts
+++ api/index.ts
@@ -1,6 +1,38 @@
-import { NowRequest, NowResponse } from "@vercel/node";
+import { NowRequest, NowResponse } from '@vercel/node';
+import axios from 'axios';
 
-export default (request: NowRequest, response: NowResponse) => {
-  const { name = "World" } = request.query;
-  response.status(200).send(`Hello ${name}!`);
+const {
+  AUTHORIZATION_TOKEN,
+  GITHUB_USER,
+  GITHUB_REPOSITORY,
+  GITHUB_TOKEN,
+  ACTIONS_EVENT_TYPE,
+} = process.env;
+
+export default async (request: NowRequest, response: NowResponse) => {
+  const { authorization } = request.headers;
+
+  if (authorization !== `Bearer ${AUTHORIZATION_TOKEN}`) {
+    response.status(403).send('Authorization failed');
+    return;
+  }
+
+  try {
+    await axios.post(
+      `https://api.github.com/repos/${GITHUB_USER}/${GITHUB_REPOSITORY}/dispatches`,
+      {
+        event_type: ACTIONS_EVENT_TYPE,
+      },
+      {
+        headers: {
+          accept: 'application/vnd.github.v3+json',
+          authorization: `token ${GITHUB_TOKEN}`,
+        },
+      }
+    );
+
+    response.status(200).send('success');
+  } catch (error) {
+    response.status(422).send(error);
+  }
 };
